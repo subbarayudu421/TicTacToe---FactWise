002f48f07cb914c6f82b85ab80aa0f23c2a4a20c
SUBBARAYUDU MV <143101061+subbarayudu421@users.noreply.github.com>
2024-07-09 13:58:21 +0530

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{tsx,ts,js,jsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
